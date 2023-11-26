b2f33c51087439182ee320d43ed7b8b6bb3df871
David A. Adams <david.adams97@gmail.com>
2023-11-25 22:55:00 -0500

    chore: update naming

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ from ytmusicapi import YTMusic
 ytmusic = YTMusic("oauth.json")
 app = Flask(__name__)
 
-@app.get("/v1/playlists")
+@app.get("/v1/library/playlists")
 def getPlaylists():
     return ytmusic.get_library_playlists()
