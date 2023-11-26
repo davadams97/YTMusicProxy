a0557878bc72f27a6a98b4e5f5a8df7112a42a78
David A. Adams <david.adams97@gmail.com>
2023-11-25 23:15:40 -0500

    feat: add user endpoint

--- app.py
+++ app.py
@@ -7,3 +7,7 @@ app = Flask(__name__)
 @app.get("/v1/library/playlists")
 def getPlaylists():
     return ytmusic.get_library_playlists()
+
+@app.get("/v1/browse/user/<channel_id>")
+def getUser(channel_id):
+    return ytmusic.get_user(channel_id)
