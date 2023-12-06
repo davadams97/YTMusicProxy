a80c3f422b015943c92319722d5b842792f81482
David A. Adams <david.adams97@gmail.com>
2023-12-05 20:40:09 -0500

    feat: add endpoints

--- app.py
+++ app.py
@@ -1,13 +1,36 @@
 from flask import Flask
+from flask import request
+
+# Third-party imports
 from ytmusicapi import YTMusic
 
 ytmusic = YTMusic("oauth.json")
 app = Flask(__name__)
 
-@app.get("/v1/library/playlists")
+@app.get("/v1/playlists")
 def getPlaylists():
     return ytmusic.get_library_playlists()
 
-@app.get("/v1/browse/user/<channel_id>")
+@app.post("/v1/playlists")
+def createPlaylist():
+    request_data = request.get_json()
+
+    title = request_data['title']
+    description = request_data.get('description', '')
+    privacy_status = request_data.get('privacyStatus', 'PRIVATE')
+    video_ids = request_data.get('videoIds', None)
+    source_playlist = request_data.get('sourcePlaylist', None)
+
+    return ytmusic.create_playlist(title, description, privacy_status, video_ids, source_playlist)
+
+@app.get("/v1/playlists/<playlist_id>")
+def getPlaylist(playlist_id):
+    return ytmusic.get_playlist(playlist_id)
+
+@app.get("/v1/users/<channel_id>")
 def getUser(channel_id):
     return ytmusic.get_user(channel_id)
+
+
+
+
