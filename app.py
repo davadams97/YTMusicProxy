7b239d951e32cca2fed8da7e2c51a4f329fb6410
David A. Adams <david.adams97@gmail.com>
2023-12-06 00:44:11 -0500

    feat: add endpoint for add songs to playlist

--- app.py
+++ app.py
@@ -27,9 +27,33 @@ def createPlaylist():
 def getPlaylist(playlist_id):
     return ytmusic.get_playlist(playlist_id)
 
+@app.post("/v1/playlists/<playlist_id>/add-songs")
+def addToPlaylist(playlist_id):
+    request_data = request.get_json()
+
+    video_ids = request_data.get('videoIds', None)
+    source_playlist = request_data.get('source_playlist', None)
+    duplicates = request_data.get('duplicates', False)
+
+    app.logger.debug('%s video ids',video_ids)
+
+    return ytmusic.add_playlist_items(playlist_id, video_ids, source_playlist, duplicates)
+
 @app.get("/v1/users/<channel_id>")
 def getUser(channel_id):
     return ytmusic.get_user(channel_id)
+
+@app.get("/v1/search")
+def search():
+    query_params = request.args
+
+    query = query_params.get('query')
+    filter = query_params.get('filter', 'songs')
+    scope = query_params.get('scope')
+    limit = query_params.get('limit', 20)
+    ignore_spelling = query_params.get('ignoreSpelling', False)
+
+    return ytmusic.search(query, filter, scope, limit, ignore_spelling)
 
 
 
