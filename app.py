8024e86f2195fa8a08f106caa9bae4c4f0b97522
David A. Adams <david@stemble.ca>
2024-01-05 16:14:12 -0500

    refactor: pass auth token
    return 401 if not authorized

--- app.py
+++ app.py
@@ -1,64 +1,95 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request, abort
 
 # Third-party imports
 from ytmusicapi import YTMusic
 
-ytmusic = YTMusic("oauth.json")
 app = Flask(__name__)
 
 @app.get("/v1/playlists")
-def getPlaylists():
-    return ytmusic.get_library_playlists()
-
+def get_playlists():
+    if 'authorization' in request.headers:
+        bearer_token = request.headers['authorization']
+        yt_music = YTMusic({'authorization': bearer_token})
+        return yt_music.get_library_playlists()
+    else:
+        abort(401, description="You are not authorized to view this content")
 @app.post("/v1/playlists")
-def createPlaylist():
-    request_data = request.get_json()
+def create_playlist():
 
-    title = request_data['title']
-    description = request_data.get('description', '')
-    privacy_status = request_data.get('privacyStatus', 'PRIVATE')
-    video_ids = request_data.get('videoIds', None)
-    source_playlist = request_data.get('sourcePlaylist', None)
+    if 'authorization' in request.headers:
+        bearer_token = request.headers['authorization']
+        yt_music = YTMusic({'authorization': bearer_token})
 
-    return ytmusic.create_playlist(title, description, privacy_status, video_ids, source_playlist)
+        request_data = request.get_json()
+
+        title = request_data['title']
+        description = request_data.get('description', '')
+        privacy_status = request_data.get('privacyStatus', 'PRIVATE')
+        video_ids = request_data.get('videoIds', None)
+        source_playlist = request_data.get('sourcePlaylist', None)
+
+        return yt_music.create_playlist(title, description, privacy_status, video_ids, source_playlist)
+    else:
+        abort(401, description="You are not authorized to view this content")
 
 @app.get("/v1/playlists/<playlist_id>")
-def getPlaylist(playlist_id):
-    return ytmusic.get_playlist(playlist_id)
+def get_playlist(playlist_id):
+    if 'authorization' in request.headers:
+        bearer_token = request.headers['authorization']
+        yt_music = YTMusic({'authorization': bearer_token})
+        return yt_music.get_playlist(playlist_id)
+    else:
+        abort(401, description="You are not authorized to view this content")
 
 @app.delete("/v1/playlists/<playlist_id>")
-def deletePlaylist(playlist_id):
-    return ytmusic.delete_playlist(playlist_id)
+def delete_playlist(playlist_id):
+    if 'authorization' in request.headers:
+        bearer_token = request.headers['authorization']
+        yt_music = YTMusic({'authorization': bearer_token})
+        return yt_music.delete_playlist(playlist_id)
+    else:
+        abort(401, description="You are not authorized to view this content")
 
-@app.post("/v1/playlists/<playlist_id>/add-songs")
-def addToPlaylist(playlist_id):
-    request_data = request.get_json()
+# TODO: update endpoint
+@app.post("/v1/playlists/<playlist_id>")
+def add_to_playlist(playlist_id):
+    if 'authorization' in request.headers:
+        bearer_token = request.headers['authorization']
+        yt_music = YTMusic({'authorization': bearer_token})
 
-    video_ids = request_data.get('videoIds', None)
-    source_playlist = request_data.get('source_playlist', None)
-    duplicates = request_data.get('duplicates', False)
+        request_data = request.get_json()
 
-    app.logger.debug('%s video ids',video_ids)
+        video_ids = request_data.get('videoIds', None)
+        source_playlist = request_data.get('source_playlist', None)
+        duplicates = request_data.get('duplicates', False)
 
-    return ytmusic.add_playlist_items(playlist_id, video_ids, source_playlist, duplicates)
+        return yt_music.add_playlist_items(playlist_id, video_ids, source_playlist, duplicates)
+    else:
+        abort(401, description="You are not authorized to view this content")
 
 @app.get("/v1/users/<channel_id>")
-def getUser(channel_id):
-    return ytmusic.get_user(channel_id)
+def get_user(channel_id):
+    if 'authorization' in request.headers:
+        bearer_token = request.headers['authorization']
+        yt_music = YTMusic({'authorization': bearer_token})
+        return yt_music.get_user(channel_id)
+    else:
+        abort(401, description="You are not authorized to view this content")
 
 @app.get("/v1/search")
 def search():
-    query_params = request.args
+    if 'authorization' in request.headers:
+        bearer_token = request.headers['authorization']
+        yt_music = YTMusic({'authorization': bearer_token})
 
-    query = query_params.get('query')
-    filter = query_params.get('filter', 'songs')
-    scope = query_params.get('scope')
-    limit = query_params.get('limit', 20)
-    ignore_spelling = query_params.get('ignoreSpelling', False)
+        query_params = request.args
 
-    return ytmusic.search(query, filter, scope, limit, ignore_spelling)
+        query = query_params.get('query')
+        filter = query_params.get('filter', 'songs')
+        scope = query_params.get('scope')
+        limit = query_params.get('limit', 20)
+        ignore_spelling = query_params.get('ignoreSpelling', False)
 
-
-
-
+        return yt_music.search(query, filter, scope, limit, ignore_spelling)
+    else:
+        abort(401, description="You are not authorized to view this content")
